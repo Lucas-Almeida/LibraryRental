f2307f8a13390ee32df8f5f872bd014e6d951a67
Lucas Pinheiro Almeida <lucas_pinheiroalmeida@hotmail.com>
2018-10-07 16:38:40 -0300

    Library Rental

---
+++ LibraryRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibraryRental.Startup))]
+namespace LibraryRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
